1e828efcec1f51872232ecf5382b3b7e5aae7365
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-17 23:51:57 +0800

    feat: set up the routes folder and modify the routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const router = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,9 +12,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(router)
 
 app.listen(port, () => {
   console.log(`app.js is running on http://localhost:${port}`)
