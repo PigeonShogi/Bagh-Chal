183f9cf6e1cb9074d6177bd580327b59dabb2cdd
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-21 11:56:40 +0800

    feat: create pieces.js model and modify home.js route

---
+++ models/pieces.js
@@ -0,0 +1,7 @@
+const pieces = {
+  tiger: '虎',
+  goat: '羊',
+  space: '空'
+}
+
+module.exports = pieces
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const pieces = require('../../models/pieces')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.get('/new', (req, res) => {
+  res.render('index', ({ pieces }))
 })
 
 module.exports = router
